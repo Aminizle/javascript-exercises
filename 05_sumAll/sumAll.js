7cf0ae24d730f4e3abb3f07d2b08a2fbb3534bfb
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-07-12 20:53:31 -0400

    added sumAll all features

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+  let smallNumber, largeNumber;
+
+  if (num1 < num2) {
+    smallNumber = num1;
+    largeNumber = num2;
+  } else {
+    smallNumber = num2;
+    largeNumber = num1;
+  }
+
+  for (let i = smallNumber; i <= largeNumber; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
